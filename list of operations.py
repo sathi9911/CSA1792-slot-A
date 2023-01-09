f84a84c7ca72581663166a8a8450d18ad0f8d79d
sathi9911 <113405350+sathi9911@users.noreply.github.com>
2023-01-09 22:27:07 +0530

    Add files via upload

---
+++ list of operations.py
@@ -0,0 +1,48 @@
+1.Repetation
+# repetition of list  
+# declaring the list  
+list1 = [12, 14, 16, 18, 20]  
+# repetition operator *  
+l = list1 * 2  
+print(l)
+
+2.concatination
+# concatenation of two lists  
+# declaring the lists  
+list1 = [12, 14, 16, 18, 20]  
+list2 = [9, 10, 32, 54, 86]  
+# concatenation operator +  
+l = list1 + list2  
+print(l)  
+
+3.length
+# size of the list  
+# declaring the list  
+list1 = [12, 14, 16, 18, 20, 23, 27, 39, 40]  
+# finding length of the list  
+len(list1)  
+
+4.iteration
+# iteration of the list  
+# declaring the list  
+list1 = [12, 14, 16, 39, 40]  
+# iterating  
+for i in list1:   
+    print(i)  
+
+5.membership
+# membership of the list  
+# declaring the list  
+list1 = [100, 200, 300, 400, 500]  
+# true will be printed if value exists  
+# and false if not  
+  
+print(600 in list1)  
+print(700 in list1)  
+print(1040 in list1)  
+  
+print(300 in list1)  
+print(100 in list1)  
+print(500 in list1)   
+
+ 
