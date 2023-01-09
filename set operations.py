aa24c078efd383c6f92cc12b894a13b3468169ea
sathi9911 <113405350+sathi9911@users.noreply.github.com>
2023-01-09 22:43:41 +0530

    Add files via upload

---
+++ set operations.py
@@ -0,0 +1,29 @@
+1. union
+# Defining the two sets
+first_set = {1, 5, 7, 4, 5}
+second_set = {4, 5, 6, 7, 8}
+ 
+# Creating the union of the two sets
+new_set = first_set | second_set
+ 
+print(new_set)
+
+2.intersection
+# Defining the two sets
+first_set = {1, 5, 7, 4, 5}
+second_set = {4, 5, 6, 7, 8}
+ 
+# Creating the intersection of the two sets
+new_set = first_set & second_set
+ 
+print(new_set)
+
+3. set difference
+# Defining the two sets
+first_set = {1, 5, 7, 4, 5}
+second_set = {4, 5, 6, 7, 8}
+ 
+# Creating the difference of the two sets
+new_set = first_set - second_set
+ 
+print(new_set)
